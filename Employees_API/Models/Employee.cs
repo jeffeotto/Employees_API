c8f05fd6790d145cc6a5198abd49e654a0265594
Jeffe Otto <fesotto@yahoo.es>
2020-09-22 12:22:08 +0200

    Db Context
    create Employee class properties,
    create dbcontext class EmployeeContext

--- Employees_API/Models/Employee.cs
+++ Employees_API/Models/Employee.cs
@@ -3,8 +3,11 @@ namespace Employees_API.Models
 {
     public class Employee
     {
-        public Employee()
-        {
-        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+       
     }
 }
---
+++ Employees_API/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace Employees_API.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeContext> Employees { get; set; }
+    }
+}
